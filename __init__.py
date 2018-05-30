8095e1ded01a617fe66c87e94a9457cc784882f3
Brick <samuelwilde@hotmail.com>
2018-05-30 17:28:48 +0100

    Disabled automatically loading ObfuArchHook, allowing for faster initial analysis

--- __init__.py
+++ __init__.py
@@ -1,15 +1,7 @@
-from binaryninja import Architecture, PluginCommand
+from binaryninja import Architecture, PluginCommand, log
 from obfu_hook import ObfuArchHook
 from obfu_passes import fix_obfuscation
 from obfu_utils import RunInBackground
-
-obfu_arches = [
-    'x86',
-    'x86_64'
-]
-
-for arch in obfu_arches:
-    ObfuArchHook(Architecture[arch]).register()
 
 
 def fix_obfuscation_command(view, func):
@@ -17,9 +9,21 @@ def fix_obfuscation_command(view, func):
     task.start()
 
 
+def load_arch_hook_command(view):
+    ObfuArchHook(view.arch).register()
+    log.log_info('Loaded ObfuArchHook for {0}'.format(view.arch.name))
+
+
 PluginCommand.register_for_function(
     'Fix Obfuscation',
     'Fix certain obfuscation methods',
     lambda view, func: fix_obfuscation_command(view, func),
-    lambda view, func: view.arch.name in obfu_arches
+    lambda view, func: type(view.arch) == ObfuArchHook
+)
+
+PluginCommand.register(
+    'Load ObfuArchHook',
+    'Loads ObfuArchHook',
+    lambda view: load_arch_hook_command(view),
+    lambda view: type(view.arch) != ObfuArchHook
 )
